653096a1c177d92ab1353fe6f557014457ad5fcb
osmanbeshirov <osman.beshirov@gmail.com>
2022-11-20 01:07:46 +0300

    basic menu layout is added

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import '../Navbar/styles.css'
+
 const Navbar = () => {
     return (
         <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
+            <nav className='nav'>
+                <div className='left'>
+                    <div className="logo">
+                        <Link to={'/'} >E-commerce</Link>
+                    </div>
 
-                </ul>
+                    <ul className="menu">
+                        <li><Link to={'products'}>Products</Link></li>
+                    </ul>
+                </div>
+
+                <div className='right'>
+                    right
+                </div>
+
+
             </nav>
         </div>
     )
