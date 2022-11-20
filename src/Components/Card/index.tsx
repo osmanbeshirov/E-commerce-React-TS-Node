50084fec06ae2bb28e228c1ed8f0aa72cf6eb1a3
osmanbeshirov <osman.beshirov@gmail.com>
2022-11-21 00:07:04 +0300

    card component

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,10 +1,39 @@
 import { chakra } from '@chakra-ui/react'
 import React from 'react'
-import {Box, Image, Button } from '@chakra-ui/react'
+
+import { Box, Image, Flex, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Card = () => {
     return (
-        <div>Card</div>
+        <div>
+            <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p='3'>
+
+                <Link to={'#'}>
+                    <Image src='https://picsum.photos/id/8/400/200' alt='product' />
+
+                    <Box p='6' >
+
+                        <Flex>
+                            <Box alignItems={'baseline'}>
+                                12/12/2022
+                            </Box>
+                        </Flex>
+
+                        <Box mt={'1'} fontWeight='semibold' as='h4' lineHeight={'tight'} >
+                            MacBook Pro
+                        </Box>
+
+                        <Box>100 TL</Box>
+                    </Box>
+                </Link>
+
+                <Button colorScheme={'blue'}>
+                    Add to Basket
+                </Button>
+
+            </Box>
+        </div>
     )
 }
 
--- src/Pages/Products/index.tsx
+++ src/Pages/Products/index.tsx
@@ -1,41 +1,21 @@
 import Card from '../../Components/Card/index'
 import React from 'react'
-import { Box, Image, Flex, Button } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
+import { Grid, GridItem } from "@chakra-ui/react"
+
 
 const Products = () => {
     return (
-        <Box borderWidth='1px' borderRadius='lg' overflow='hidden' p='3'>
-
-            <Link to={'#'}>
-                <Image src='https://picsum.photos/id/8/400/200' alt='product' />
-
-                <Box p='6' >
-
-                    <Flex>
-                        <Box alignItems={'baseline'}>
-                            12/12/2022
-                        </Box>
-
-                    </Flex>
-
-                    <Box mt={'1'} fontWeight='semibold' as='h4' lineHeight={'tight'} >
-                        MacBook Pro
-                    </Box>
-
-                    <Box>100 TL</Box>
+        <>
 
 
+            <Grid templateColumns='repeat(3, 1fr)' gap={3}>
+                <Card />
+                <Card />
+                <Card />
+                
+            </Grid>
 
-
-                </Box>
-            </Link>
-
-            <Button colorScheme={'blue'}>
-                Add to Basket
-            </Button>
-
-        </Box>
+        </>
     )
 }
 
